a3ce857fd32bd50cb999f100a16ba183ea43d156
Dennis Ocaña <dennisocana@gmail.com>
2023-05-05 19:40:26 -0500

    dsa

---
+++ AplicativoSeguridad/Data/Migrations/20230506001537_BddActivos.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AplicativoSeguridad.Data.Migrations
+{
+    public partial class BddActivos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Activo",
+                columns: table => new
+                {
+                    ActivoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Identificador = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ubicacion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Proceso = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NombreActivo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Responsable = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Clasificacion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ValEconomico = table.Column<int>(type: "int", nullable: false),
+                    ValOps = table.Column<int>(type: "int", nullable: false),
+                    ValLegal = table.Column<int>(type: "int", nullable: false),
+                    ValRep = table.Column<int>(type: "int", nullable: false),
+                    ValPriv = table.Column<int>(type: "int", nullable: false),
+                    ValSeg = table.Column<int>(type: "int", nullable: false),
+                    Criticidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activo", x => x.ActivoID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Activo");
+        }
+    }
+}
