c9e00e8ba619006b7f217c0e0e94f9478f27baf0
Dennis Ocaña <dennisocana@gmail.com>
2023-05-05 19:30:31 -0500

    f

--- AplicativoSeguridad/Models/Activo.cs
+++ AplicativoSeguridad/Models/Activo.cs
@@ -5,31 +5,18 @@ namespace AplicativoSeguridad.Models
     public class Activo
     {
         public int ActivoID { get; set; }
-        [Required]
         public string? Identificador { get; set; }
-        [Required]
         public string? Ubicacion { get; set; }
-        [Required]
         public string? Proceso { get; set; }
-        [Required]
         public string? NombreActivo { get; set; }
-        [Required]
         public string? Descripcion { get; set; }
-        [Required]
         public string? Responsable { get; set; }
-        [Required]
         public string? Clasificacion { get; set; }
-        [Required]
         public int ValEconomico {  get; set; }
-        [Required]
         public int ValOps { get; set; }
-        [Required]
         public int ValLegal { get; set; }
-        [Required]
         public int ValRep { get; set; }
-        [Required]
         public int ValPriv { get; set; }
-        [Required]
         public int ValSeg { get; set; }
         public int Criticidad { get; set; }
     }
